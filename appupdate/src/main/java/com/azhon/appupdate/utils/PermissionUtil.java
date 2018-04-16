fe4089696193f856c79ca02d7c0d07d357786ecd
阿钟 <958460248@qq.com>
2018-04-16 15:29:26 +0800

    修改函数名称 添加权限检查

--- appupdate/src/main/java/com/azhon/appupdate/utils/PermissionUtil.java
+++ appupdate/src/main/java/com/azhon/appupdate/utils/PermissionUtil.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
 
 /**
@@ -58,5 +59,15 @@ public final class PermissionUtil {
     @RequiresApi(api = Build.VERSION_CODES.O)
     public static boolean checkOreoInstallPermission(Context context) {
         return context.getPackageManager().canRequestPackageInstalls();
+    }
+
+    /**
+     * 检查通知权限
+     *
+     * @return false 没有权限，需要申请
+     */
+    public static boolean checkNotificationPermission(Context context) {
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
+        return managerCompat.areNotificationsEnabled();
     }
 }
