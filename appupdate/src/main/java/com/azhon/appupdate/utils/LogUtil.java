052b703e9f3aef452d602ae3198fb4fcbba28587
阿钟 <958460248@qq.com>
2018-11-09 14:04:41 +0800

    - 生成java doc文档供参考

--- appupdate/src/main/java/com/azhon/appupdate/utils/LogUtil.java
+++ appupdate/src/main/java/com/azhon/appupdate/utils/LogUtil.java
@@ -17,54 +17,105 @@ import com.azhon.appupdate.manager.DownloadManager;
 
 public final class LogUtil {
 
-    private static boolean b = true;
+    /**
+     * 日志开关
+     */
+    private static boolean b;
 
     static {
         b = DownloadManager.getInstance().getConfiguration().isEnableLog();
     }
 
+    /**
+     * 输出Error信息
+     *
+     * @param tag tag
+     * @param msg String
+     */
     public static void e(String tag, String msg) {
         if (b) {
             Log.e(tag, msg);
         }
     }
 
+    /**
+     * 输出Error信息
+     *
+     * @param tag tag
+     * @param msg int
+     */
     public static void e(String tag, int msg) {
         if (b) {
             Log.e(tag, String.valueOf(msg));
         }
     }
 
+    /**
+     * 输出Error信息
+     *
+     * @param tag tag
+     * @param msg float
+     */
     public static void e(String tag, float msg) {
         if (b) {
             Log.e(tag, String.valueOf(msg));
         }
     }
 
+    /**
+     * 输出Error信息
+     *
+     * @param tag tag
+     * @param msg Long
+     */
     public static void e(String tag, Long msg) {
         if (b) {
             Log.e(tag, String.valueOf(msg));
         }
     }
 
+    /**
+     * 输出Error信息
+     *
+     * @param tag tag
+     * @param msg double
+     */
     public static void e(String tag, double msg) {
         if (b) {
             Log.e(tag, String.valueOf(msg));
         }
     }
 
+    /**
+     * 输出Error信息
+     *
+     * @param tag tag
+     * @param msg boolean
+     */
     public static void e(String tag, boolean msg) {
         if (b) {
             Log.e(tag, String.valueOf(msg));
         }
     }
 
+    /**
+     * 输出Debug信息
+     *
+     * @param tag tag
+     * @param msg String
+     */
     public static void d(String tag, String msg) {
         if (b) {
             Log.d(tag, msg);
         }
     }
 
+    /**
+     * 输出Info信息
+     *
+     * @param tag tag
+     * @param msg 字符串
+     */
     public static void i(String tag, String msg) {
         if (b) {
             Log.i(tag, msg);
