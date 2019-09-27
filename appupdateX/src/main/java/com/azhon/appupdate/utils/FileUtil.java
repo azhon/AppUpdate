43d57870124ba621590e8ab0d973d5d47b079251
阿钟 <958460248@qq.com>
2019-09-28 00:12:11 +0800

    Util 新增文件MD5获取

--- appupdateX/src/main/java/com/azhon/appupdate/utils/FileUtil.java
+++ appupdateX/src/main/java/com/azhon/appupdate/utils/FileUtil.java
@@ -1,8 +1,11 @@
 package com.azhon.appupdate.utils;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 
 /**
  * 项目名:    AppUpdate
@@ -71,5 +74,29 @@ public final class FileUtil {
      */
     public static boolean delete(String downloadPath, String fileName) {
         return new File(downloadPath, fileName).delete();
+    }
+
+    /**
+     * 获取一个文件的MD5
+     *
+     * @param file 文件
+     * @return MD5
+     */
+    public static String getFileMD5(File file) {
+        try {
+            byte[] buffer = new byte[1024];
+            int len;
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            FileInputStream in = new FileInputStream(file);
+            while ((len = in.read(buffer)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+            in.close();
+            BigInteger bigInt = new BigInteger(1, digest.digest());
+            return bigInt.toString(16).toUpperCase();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }
